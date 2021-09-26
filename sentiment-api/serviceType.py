13d4a29a31a81bf5f3018e2bd55237035dd1e5a9
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-09-26 09:12:27 +0700

    add regex

--- sentiment-api/serviceType.py
+++ sentiment-api/serviceType.py
@@ -1,11 +1,19 @@
 import re
 
-pattern = "^พัสดุ|พัสดุ|พัสดุ$"
+parcelPattern = "^พัสดุ|พัสดุ|พัสดุ$"
+deliverlyServicePatten = "^เรียกรถ|รถ|รถเข้ารับพัสดุ$"
+servicePattern = "^บริการ|บริการ|บริการจากเรา^|^ตรวจสอบค่าบริการ$|^.*ราคา$|^ราคา|ราคา$"
+promotionPattern = "^โปรโมชั่น|โปรโมชั่น|^รายละเอียดโปรโมชั่น$|^โปรโมชั่น$"
 
 def service_type(sentimentText) -> str:
-  if re.findall(pattern,sentimentText) or re.match(pattern, sentimentText):
-    return "บริการ"
+  if re.findall(parcelPattern,sentimentText) or re.match(parcelPattern, sentimentText):
+    return "บริการด้านพัสดุ"
+  elif re.findall(deliverlyServicePatten,sentimentText) or re.match(deliverlyServicePatten, sentimentText):
+    return "บริการด้านเรียกรถ"
+  elif re.findall(servicePattern,sentimentText) or re.match(servicePattern, sentimentText):
+    return "ด้านบริการ"
+  elif re.findall(promotionPattern,sentimentText) or re.match(promotionPattern, sentimentText):
+    return "ด้านโปรโมชั่น"
   else:
     return "อื่นๆ"
-
 
