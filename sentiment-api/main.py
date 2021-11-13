a649b1dcaddd1628c730f3ac5a0aecb2948c9720
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-11-14 02:31:18 +0700

    add lstm predict

--- sentiment-api/main.py
+++ sentiment-api/main.py
@@ -3,12 +3,76 @@ from fastapi import FastAPI, HTTPException
 from serviceType import service_type
 from Models.SentimentData import SentimentData
 from Service.init_csv import initial_csv
+import logging
 from fastapi.logger import logger
+from pythainlp import word_tokenize
+import re
+from utils import cleansing
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+load_model = tf.keras.models.load_model
 
 app = FastAPI()
 
+gunicorn_logger = logging.getLogger('gunicorn.error')
+logger.handlers = gunicorn_logger.handlers
+if __name__ != "main":
+    logger.setLevel(gunicorn_logger.level)
+else:
+    logger.setLevel(logging.DEBUG)
+
 vector = load("vectors.joblib")
 model = load("model.joblib")
+
+def loadModel():
+  global predict_model
+  predict_model = load_model('./Algorithm/lstm.h5')
+
+loadModel()
+
+df = pd.read_csv('./data/data.csv', sep=',').drop_duplicates(subset=['Sentiment', 'SentimentText'], keep=False)
+df = df.reset_index(drop=True)
+df['Sentiment'] = df['Sentiment'].map({0: 'Negative', 1: 'Neutral'})
+sentiment = df.SentimentText.values
+category = df.Sentiment.values
+unique_category = list(set(category))
+cleaned_words, temp = cleansing.cleaning(sentiment)
+max_length = cleansing.max_length(temp)
+print('...cleansing data...')
+logger.info('...cleansing data...')
+cleaned_words, temp = cleansing.cleaning(sentiment)
+print('...done...')
+logger.info('...done...')
+predict_word_tokenizer = cleansing.create_tokenizer(cleaned_words)
+encoded_doc = cleansing.encoding_doc(predict_word_tokenizer, cleaned_words)
+padded_doc = cleansing.padding_doc(encoded_doc, max_length)
+
+def predictLSTM(text):
+  clean = re.sub(r'[^ก-๙]', " ", text)
+  test_word = word_tokenize(clean)
+  test_word = [w.lower() for w in test_word]
+  test_ls = predict_word_tokenizer.texts_to_sequences(test_word)
+  print(test_word)
+  if [] in test_ls:
+    test_ls = list(filter(None, test_ls))
+  test_ls = np.array(test_ls).reshape(1, len(test_ls))
+  x = cleansing.padding_doc(test_ls, max_length)
+  pred = predict_model.predict(x)
+
+  return pred
+
+def get_final_output(pred, classes):
+  predictions = pred[0]
+  classes = np.array(classes)
+  ids = np.argsort(-predictions)
+  classes = classes[ids]
+  
+  predictions = -np.sort(-predictions)
+  
+  for i in range(pred.shape[1]):
+    print("%s has confidence = %s" % (classes[i], (predictions[i])))
+    return classes[i], predictions[i]
 
 @app.get("/")
 async def read_root():
@@ -22,6 +86,13 @@ async def get_predict(sentimentText: str):
   prediction = model.predict(vec)  
   return {"Sentiment" : sentimentText, "Predict": prediction[0], "Service Type": guard}
 
+@app.post('/predict-lstm')
+async def get_lstm_predict(sentimentText: str):
+  guard = service_type(sentimentText)
+  pred = predictLSTM(sentimentText)
+  res = get_final_output(pred, unique_category)
+  return {"Sentiment" : sentimentText, "Predict": res[0], "Service Type": guard}
+
 @app.post("/data", response_model=SentimentData, status_code=200)
 async def get_data(sentiment: SentimentData) -> SentimentData:
   logger.info(f"receive data {sentiment}")
@@ -30,41 +101,3 @@ async def get_data(sentiment: SentimentData) -> SentimentData:
   data = [sentiment.Sentiment, sentiment.SentimentText]
   await initial_csv(data)
   return sentiment
-
-# from pydantic import BaseModel
-# import tensorflow as tf
-# load_model = tf.keras.models.load_model
-# import numpy as np
-# class Data(BaseModel):
-#     text: str
-
-# def loadModel():
-#     global predict_model
-
-#     predict_model = load_model('./Algorithm/lstm.h5')
-
-# loadModel()
-
-# async def predict(text):
-#     clean = re.sub(r'[^ก-๙]', " ", text)
-#     test_word = word_tokenize(clean)
-#     test_word = [w.lower() for w in test_word]
-#     test_ls = train_word_tokenizer.texts_to_sequences(test_word)
-#     print(test_word)
-#     if [] in test_ls:
-#       test_ls = list(filter(None, test_ls))
-      
-#     test_ls = np.array(test_ls).reshape(1, len(test_ls))
-  
-#     x = padding_doc(test_ls, max_length)
-    
-#     pred = predict_model.predict(x)
-  
-  
-#   return pred
-
-# @app.post('/getclass/')
-# async def get_class(data: Data):
-#     category, confidence = await predict(data)
-#     res = {'class': category, 'confidence':confidence}
-#     return {'results': res}
---
+++ sentiment-api/utils/cleansing.py
@@ -0,0 +1,32 @@
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import re
+from pythainlp import word_tokenize
+from pythainlp.corpus.common import thai_stopwords
+thai_stopwords = list(thai_stopwords())
+from string import punctuation
+
+def cleaning(sentences):
+    words = []
+    temp = []
+    for s in sentences:
+        clean = re.sub(r'[^ก-๙]', "", s)
+        w = word_tokenize(clean, engine='deepcut')
+        temp.append([i.lower() for i in w])
+        words.append(' '.join(word for word in w if word not in thai_stopwords and word not in punctuation and word not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ", "|")))
+    
+    return words, temp
+
+def create_tokenizer(words, filters = ''):
+    token = Tokenizer(filters=filters)
+    token.fit_on_texts(words)
+    return token
+
+def encoding_doc(token, words):
+    return(token.texts_to_sequences(words))
+
+def padding_doc(encoded_doc, max_length):
+    return(pad_sequences(encoded_doc, maxlen = max_length, padding = "post"))
+
+def max_length(words):
+    return(len(max(words, key = len)))
