2ff4f7c73f8b015b67db64c34106649dd490e0ca
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2021-11-17 10:25:21 +0700

    fix models#2

--- sentiment-api/utils/cleansing.py
+++ sentiment-api/utils/cleansing.py
@@ -5,6 +5,12 @@ from pythainlp import word_tokenize
 from pythainlp.corpus.common import thai_stopwords
 thai_stopwords = list(thai_stopwords())
 from string import punctuation
+from pythainlp.ulmfit import (process_thai,
+    replace_rep_after,
+    fix_html,
+    ungroup_emoji,
+    replace_wrep_post,
+    remove_space)
 
 def cleaning(sentences):
     words = []
@@ -12,10 +18,15 @@ def cleaning(sentences):
     for s in sentences:
         clean = re.sub(r'[^ก-๙]', "", s)
         # w = word_tokenize(clean, engine='deepcut')
-        w = word_tokenize(clean, engine='attacut')
+        # w = word_tokenize(clean, engine='attacut')
+        w = process_thai(clean,
+                pre_rules=[replace_rep_after, fix_html],
+                    post_rules=[ungroup_emoji,
+                                replace_wrep_post,
+                                remove_space])
         temp.append([i.lower() for i in w])
-        words.append(' '.join(word for word in w if word not in thai_stopwords and word not in punctuation and word not in ("?", ".", ";", ":", "!", '"', "ๆ", "ฯ", "|")))
-    
+        words.append(' '.join(w).lower())
+
     return words, temp
 
 def create_tokenizer(words, filters = ''):
