126b5a7cd927beaabb63e9bf028a95b8583d2fa0
DESKTOP-MJV0GUE\Thiti <thiti180536@gmail.com>
2022-01-10 15:45:43 +0700

    remove replace numurical

--- sentiment-api/utils/cleansing.py
+++ sentiment-api/utils/cleansing.py
@@ -35,12 +35,12 @@ def cleansingTextToken(text):
                             replace_wrep_post_nonum,
                             remove_space]
                         )
-    text = ''.join([i for i in text])
-    text = re.sub(r'[a-z0-9]', '', text)
-    text = process_thai(text,
-                            pre_rules=[replace_rep_after, fix_html, rm_useless_spaces],
-                            post_rules=[ungroup_emoji,
-                            replace_wrep_post_nonum,
-                            remove_space]
-                        )
+    # text = ''.join([i for i in text])
+    # text = re.sub(r'[a-z0-9]', '', text)
+    # text = process_thai(text,
+    #                         pre_rules=[replace_rep_after, fix_html, rm_useless_spaces],
+    #                         post_rules=[ungroup_emoji,
+    #                         replace_wrep_post_nonum,
+    #                         remove_space]
+    #                     )
     return text
